74ee67dcc1afbf5ed9ccf1fb0203945e22242462
IGOR <sahmen@mail.ru>
2023-04-11 23:25:03 +0300

    DZ 1

---
+++ Task1.py
@@ -0,0 +1,18 @@
+def poems(str):
+    str = str.split()
+    list_1 = []
+    for word in str:
+        sum = 0
+
+        for i in word:
+            if i in 'аеёиоуыэюя':
+                sum += 1
+        list_1.append(sum)
+   
+    return len(list_1) == list_1.count(list_1[0])
+
+text = "пара-ра-рам рам-пам-папам па-ра-па-да"
+if poems(text):
+    print('Парам пам-пам')
+else:
+    print('Пам парам')
